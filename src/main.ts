34572fcddad5942a2df63a45cb005d9e5d835662
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-08-30 10:48:33 +0200

    a bit further

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,41 @@
 import './style.css'
+
+type projects = {
+    title: string,
+    category: string,
+    description: string,
+    url: URL
+}
+
+const projects: projects[] = [
+    {
+        title: 'Project 1',
+        category: 'Web Development',
+        description: 'This is a project description',
+        url: new URL('https://www.google.com')
+    },
+    {
+        title: 'Project 2',
+        category: 'Web Development',
+        description: 'This is a project description',
+        url: new URL('https://www.google.com')
+    },
+    {
+        title: 'Project 3',
+        category: 'Web Development',
+        description: 'This is a project description',
+        url: new URL('https://www.google.com')
+    },
+    {
+        title: 'Project 4',
+        category: 'Web Development',
+        description: 'This is a project description',
+        url: new URL('https://www.google.com')
+    },
+    {
+        title: 'Project 5',
+        category: 'Web Development',
+        description: 'This is a project description',
+        url: new URL('https://www.google.com')
+    },
+]
