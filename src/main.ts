eca2c299a968a6c62ef7bce45cafd2f9f29071df
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-09-04 10:46:32 +0200

    Added server

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,11 @@ type Project = {
 }
 
 //Funksjon for å hente ut prosjekter fra JSON-fil
-const fetchProjectsFromJsonFile = async () => {
-    const response = await ('./myprojects.json');
-    const projects: Project[] = await response.json();
-    return projects;
+// const fetchProjectsFromJsonFile = async () => {
+//     const response = await ('./myprojects.json');
+//     const projects: Project[] = await response.json();
+//     return projects;
     
-}
+// }
 
 //Funksjon for å legge til prosjekter i HTML
