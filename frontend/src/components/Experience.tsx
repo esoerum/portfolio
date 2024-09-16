d1bfea9b8da132d38b41d1128a35af83ca9aa885
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-09-16 13:46:50 +0200

    Update Experience.tsx
    Testing out children-prop

--- frontend/src/components/Experience.tsx
+++ frontend/src/components/Experience.tsx
@@ -1,10 +1,12 @@
 import { ExperienceProps } from './types';
+import type { PropsWithChildren } from "react";
 
-export default function Experience(props: ExperienceProps) {
-    const { description } = props;
+export default function Experience(props: Readonly<PropsWithChildren<ExperienceProps>>) {
+    const { description, children } = props;
       return (
-        <>
+        <section>
+        {children}
           <p>{description}</p>
-          </>
+          </section>
       )
     }
