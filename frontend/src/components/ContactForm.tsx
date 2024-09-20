357162c60cbdb1b52c820988bca91d0a417982cb
Espen Sørum <espensoerum@gmail.com>
2024-09-20 13:53:13 +0200

    Update ContactForm.tsx
    Handling contactform with state

--- frontend/src/components/ContactForm.tsx
+++ frontend/src/components/ContactForm.tsx
@@ -1,27 +1,45 @@
+import { useState } from "react";
+
 export type ContactFormProps = {
-    name: string;
-    text: string;
-}
-
-const sendMessage = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-}
-
+	onContactButtonClicked: (name: string, text: string) => void;
+};
 
 export default function ContactForm(props: ContactFormProps) {
-    const { name, text } = props;
-    return (
-        <section>
-            <form onSubmit={sendMessage} className="project-form">
-                <label htmlFor="name">Name:
-                <input type="text" id="name" name="name" value={name} placeholder="John Travolta" />
-                </label>
-                <label htmlFor="text">Message:
-                <textarea id="text" name="text" value={text} placeholder="Your message here"></textarea>
-                <button type="submit">Send</button>
-                </label>
-            </form>
-        </section>
-    )
+	const { onContactButtonClicked } = props;
+	const [name, setName] = useState("");
+	const [text, setText] = useState("");
 
+	const sendMessage = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		if (!name || !text) return;
+		setName("");
+		setText("");
+		onContactButtonClicked(name, text);
+	};
+
+	return (
+		<section>
+			<form onSubmit={sendMessage} className="project-form">
+				<label htmlFor="name">Name:</label>
+				<input
+					type="text"
+					id="name"
+					name="name"
+					value={name}
+					placeholder="John Travolta"
+					onChange={(e) => setName(e.target.value)}
+				/>
+
+				<label htmlFor="text">Message:</label>
+				<textarea
+					id="text"
+					name="text"
+					value={text}
+					placeholder="Your message here"
+					onChange={(e) => setText(e.target.value)}
+				></textarea>
+				<button type="submit">Send</button>
+			</form>
+		</section>
+	);
 }
