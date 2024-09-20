89700b093e4533216b860fc46e9222f7bbd49b11
Espen Sørum <espensoerum@gmail.com>
2024-09-20 13:52:46 +0200

    Update Contact.tsx

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -1,14 +1,22 @@
+import ContactForm from './ContactForm';
 import { ContactProps } from './types'
 
 
 
  export function Contact(props: ContactProps) {
   const { email } = props;
+
+  const handleOnConctactButtonClicked = () => {
+    const contactMessage = "Thank you for your message! I will get back to you as soon as possible.";
+    return alert(contactMessage);
+  }
   
     return (
       <div>
         <h3>Contact</h3>
         <button onClick={() => alert("Contact me on: "+ email) }>Display E-mail</button>
+        <ContactForm onContactButtonClicked={handleOnConctactButtonClicked}/>
+        <pre></pre>
       </div>
     )
   }
