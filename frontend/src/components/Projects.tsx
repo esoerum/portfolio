c4913611b1003d6052a51a62a3bd500e2524c8b1
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-09-23 21:07:08 +0200

    Update Projects.tsx
    Fixed keys so they work properly and dont give error in console

--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -2,15 +2,15 @@ import Project from './Project';
 import { ProjectProps, ProjectsProps } from './types';
 import type { PropsWithChildren } from "react";
 
-export default function Projects (props: Readonly<PropsWithChildren <ProjectsProps>>) {
-    const { projects = [], children} = props;
+export default function Projects(props: Readonly<PropsWithChildren<ProjectsProps>>) {
+    const { projects = [], children } = props;
     return (
         <section>
             <h2>Projects</h2>
             {children}
-            <ul>
+            <ul className="project-ul">
                 {projects.map((project: ProjectProps) => (
-                    <Project key={project.id} title={project.title} category={project.category} description={project.description} url={project.url} />
+                    <li key={project.id || project.title}><Project id={project.id} title={project.title} category={project.category} description={project.description} url={project.url} /></li>
                 ))}
             </ul>
         </section>
