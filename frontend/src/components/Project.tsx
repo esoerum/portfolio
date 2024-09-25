d26e7965116061f0ecca4fd13c12270e1e4b4af1
Espen Sørum <espensoerum@gmail.com>
2024-09-25 13:33:00 +0200

    Update Project.tsx
    Added children to project component

--- frontend/src/components/Project.tsx
+++ frontend/src/components/Project.tsx
@@ -1,13 +1,15 @@
 import { ProjectProps } from './types';
+import type { PropsWithChildren } from "react";
 
-export default function Project(props: ProjectProps) {
-    const { title, category, description, url } = props
+export default function Project(props: Readonly<PropsWithChildren<ProjectProps>>) {
+    const { title, category, description, url, children } = props
     return (
        <article className='project'>
            <h3>{title}</h3>
            <p>Category: {category}</p>
            <p>{description}</p>
            <a href={url}>View Project</a>
+           {children}
        </article>
     )
 }
