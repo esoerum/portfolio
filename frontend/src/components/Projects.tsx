e8864fa19bc547627367f8e9f763a637296c2964
Espen Sørum <espensoerum@gmail.com>
2024-09-25 13:32:43 +0200

    Update Projects.tsx

--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -1,38 +1,59 @@
-import CreateProjectForm from './CreateProjectForm';
-import Project from './Project';
-import { ProjectProps, ProjectsProps } from './types';
+import Project from "./Project";
+import { ProjectProps, ProjectsProps } from "./types";
 import type { PropsWithChildren } from "react";
 
-export default function Projects(props: Readonly<PropsWithChildren<ProjectsProps>>) {
-    const { projects = [], children } = props;
+export default function Projects(
+	props: Readonly<PropsWithChildren<ProjectsProps>>
+) {
+	const { projects = [], children, onRemoveProjectButtonClicked } = props;
 
-    const handleOnCreateProjectButtonClicked = (title: string, category: string, description: string, url: string) => {
-        console.log("Project created: ", title, category, description, url);
-    }
-
-    return (
-        <section>
-            <h2>Projects</h2>
-            {children}
-            {projects.length === 0 ? (
-                <p>No projects available.</p>
-            ) : (
-                <ul className="project-ul">
-                    {projects.map((project: ProjectProps) => (
-                        <li key={project.id || project.title}>
-                            <Project 
-                                id={project.id} 
-                                title={project.title} 
-                                category={project.category} 
-                                description={project.description} 
-                                url={project.url} 
-                            />
-                        </li>
-                    ))}
-                </ul>
-            )}
-            <CreateProjectForm onCreateProjectButtonClicked={handleOnCreateProjectButtonClicked} />
-            
-        </section>
-    )
+	return (
+		<section>
+			<h2>Projects</h2>
+			{children}
+			{projects.length === 0 ? (
+				<p>No projects available.</p>
+			) : (
+				<>
+					<ul className="project-ul">
+						{projects.map((project: ProjectProps) => (
+							<li key={project.id || project.title}>
+								<Project
+									id={project.id}
+									title={project.title}
+									category={project.category}
+									description={project.description}
+									url={project.url}
+								>
+									<button
+										onClick={() =>
+											onRemoveProjectButtonClicked(
+												project.id
+											)
+										}
+									>
+										Remove project
+									</button>
+								</Project>
+							</li>
+						))}
+					</ul>
+                    Total projects per category:
+					<ul>
+						{Object.entries(
+							projects.reduce((prev, curr) => {
+								prev[curr.category] =
+									(prev[curr.category] || 0) + 1;
+								return prev;
+							}, {} as Record<string, number>)
+						).map(([category, count]) => (
+							<li key={category}>
+								{category}: {count}
+							</li>
+						))}
+					</ul>
+				</>
+			)}
+		</section>
+	);
 }
