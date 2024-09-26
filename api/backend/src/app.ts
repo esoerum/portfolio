f2b2b77e748ee125ad576a53aaec8a99c3869400
Espen Sørum <espensoerum@gmail.com>
2024-09-26 11:04:46 +0200

    Update app.ts

--- api/backend/src/app.ts
+++ api/backend/src/app.ts
@@ -5,34 +5,34 @@ const app = new Hono();
 
 app.use("/*", cors());
 
-const dummyData = [
+let dummyData = [
     {
         id: "1",
         title: "Web applications",
         description: "Frontend and backend",
         createdAt: new Date("2024-01-01"),
-        categories: ["web, react, node, fullstack"],
+        category: ["react"],
         },
         {
         id: "2",
         title: "Python",
         description: "Scripting and automation",
         createdAt: new Date("2024-01-04"),
-        categories: ["python", "scripting, automation, programming, backend"],
+        category: ["scripting"],
         },
         {
         id: "3",
         title: "Backend development with Java", 
         description: "Spring Boot and RESTful APIs",
         createdAt: new Date("2024-01-07"),
-        categories: ["java", "backend, restful, spring boot"],
+        category: ["java"],
         },
         {
         id: "4",
         title: "Cloud computing",
         description: "AWS, GCP, Azure",
         createdAt: new Date("2024-01-02"),
-        categories: ["cloud, aws, gcp, azure"],
+        category: ["cloud"],
         },
 ]
 
@@ -41,5 +41,19 @@ app.get("/projects", (c) => {
     return c.json(dummyData);
 });
 
+app.put("/projects", async (c) => {
 
+    let project = await c.req.json();
+    project.createdAt = new Date();
+    //Validering
+    
+    dummyData.push(project);
+});
+
+
+app.delete("/projects/:id", (c) => {
+    const id = c.req.param("id");
+    dummyData = dummyData.filter((project) => project.id !== id);
+    return c.json({ message: "Project deleted" });
+});
 export default app
