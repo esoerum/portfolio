00cdd40a9075bfdee662fad5704e6d228a0d1cfd
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-10-15 15:33:46 +0200

    Refactoring App.tsx
    Added logic from app.tsx to ProjectsPage.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,13 @@ import "/src/styles/App.css";
 import IntroHeader from "./features/intro/components/IntroHeader.tsx";
 import Experiences from "./features/experiences/components/Experiences.tsx";
 import { Contact } from "./features/intro/components/Contact.tsx";
-import { ProjectProps } from "./features/projects/components/Project.tsx";
 import { ExperienceProps } from "./features/experiences/components/Experience.tsx";
-import Projects from "./features/projects/components/Projects.tsx";
-import CreateProjectForm from "./features/projects/components/CreateProjectForm.tsx";
-import { useEffect, useState } from "react";
-import { ofetch } from "ofetch";
+import ProjectsPage from "./features/projects/pages/index.tsx";
 
 //Flytte ut hardkodede data til en egen fil, config fil eller lignende
 const student = "Espen Sørum";
 const degree = "Bachelor in Informatics";
+
 const points = 180;
 const experiences: ExperienceProps[] = [
 	{ id: 1, description: "Creating frameworks with .NET and C#" },
@@ -22,69 +19,6 @@ const experiences: ExperienceProps[] = [
 const email = "student@hiof.no";
 
 function App() {
-	const [projectsList, setProjectsList] = useState<ProjectProps[]>([]);
-	//Initializing the data from the server
-	//Endre feilhåndtering til å bruke state eller try/catch
-	const initializeData = () => {
-		console.log("fetching data");
-		ofetch("http://localhost:3000/projects")
-			.then((projects: ProjectProps[]) => {
-				console.log("data fetched");
-				setProjectsList(projects);
-				console.log("data initialized");
-			})
-			.catch((error) => {
-				console.error("Error fetching projects:", error);
-			});
-	};
-	useEffect(() => {
-		initializeData();
-	}, []);
-
-	//Handling the creation of a new project
-	const handleOnCreateProjectButtonClicked = (
-		title: string,
-		category: string,
-		description: string,
-		url: string
-	  ) => {
-		console.log("Project created: ", title, category, description, url);
-		const project = {
-		  id: crypto.randomUUID(),
-		  title: title,
-		  category: category,
-		  description: description,
-		  url: url,
-		};
-		ofetch("http://localhost:3000/projects", {
-		  method: "PUT",
-		  body: JSON.stringify(project),
-		  headers: {
-			"Content-Type": "application/json",
-		  },
-		})
-		  .then((response: { message: string; project: ProjectProps }) => {
-			console.log(response.message);
-			initializeData();
-		  })
-		  .catch((error) => {
-			console.error("Error creating project:", error);
-		  });
-	  };
-	//Handling the removal of a project
-	const handleOnRemoveProjectButtonClicked = (id: string) => {
-		console.log("Project removed: ", id);
-		ofetch(`http://localhost:3000/projects/${id}`, { method: "DELETE" })
-		  .then((response: { message: string }) => {
-			console.log(response.message);
-			setProjectsList((projectsList) =>
-			  projectsList.filter((project) => project.id !== id)
-			);
-		  })
-		  .catch((error) => {
-			console.error("Error deleting project:", error);
-		  });
-	};
 	return (
 		<> {/* Eventuelt flytte ut header, experiences og contact. Disse gjenbrukes ikke, kanskje de skal pakkes inn i en intro-komponent*/}
 			<IntroHeader student={student} degree={degree} points={points} />
@@ -92,19 +26,7 @@ function App() {
 				{/* <p>Children element if needed later</p> */}
 			</Experiences>
 			<Contact email={email} />
-			<Projects
-				projects={projectsList}
-				onRemoveProjectButtonClicked={
-					handleOnRemoveProjectButtonClicked
-				}
-			>
-				{/* <p>Children element if needed later</p> */}
-			</Projects>
-			<CreateProjectForm
-				onCreateProjectButtonClicked={
-					handleOnCreateProjectButtonClicked
-				}
-			/>
+			<ProjectsPage />
 		</>
 	);
 }
