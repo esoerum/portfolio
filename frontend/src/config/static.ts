0edf247cb57e6504b3de8b317d83fa7eacc387e1
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-10-15 20:03:36 +0200

    Static data
    Added static data to config file

---
+++ frontend/src/config/static.ts
@@ -0,0 +1,15 @@
+//Static data for the frontend
+
+export const config = {
+student: "Espen Sørum",
+degree: "Bachelor in Informatics",
+email: "student@hiof.no",
+points: 180,
+experiences: [
+	{ id: 1, description: "Creating frameworks with .NET and C#" },
+	{ id: 2, description: "Javascript / Typescript and React" },
+	{ id: 3, description: "Backend development with Java" },
+	{ id: 4, description: "Python" },
+],
+
+}
