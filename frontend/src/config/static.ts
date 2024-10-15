2ab61bfc122e99d33ed280f811861bf7df02d6d2
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-10-15 22:52:36 +0200

    Update static.ts

--- frontend/src/config/static.ts
+++ frontend/src/config/static.ts
@@ -2,14 +2,15 @@
 
 export const config = {
 student: "Espen Sørum",
-degree: "Bachelor in Informatics",
+degree: "Bachelor degree in Computer Science",
 email: "student@hiof.no",
 points: 180,
 experiences: [
 	{ id: 1, description: "Creating frameworks with .NET and C#" },
 	{ id: 2, description: "Javascript / Typescript and React" },
 	{ id: 3, description: "Backend development with Java" },
-	{ id: 4, description: "Python" },
+	{ id: 4, description: "Python scripting" },
+	{ id: 5, description: "Database development (MYSQL)" },
 ],
 
 }
