f54437b096bf19d99311870f6b217fa5e6523060
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-10-16 12:28:34 +0200

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="footer">
-            <p>&copy; 2024 Espen Sørum</p>
+        <p>&copy; 2024 Espen Sørum</p>
         </footer>
     );
 }
