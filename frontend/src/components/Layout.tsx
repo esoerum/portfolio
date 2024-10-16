1894403919f9ae4b584cc0ec06ede7b0c737d8e0
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-10-16 12:28:40 +0200

    Update Layout.tsx

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,16 +2,20 @@ import type { PropsWithChildren } from "react";
 import Footer from "./Footer";
 import Navigation from "./Navigation";
 
+
 export default function Layout(props: PropsWithChildren<{}>) {
     const { children } = props;
 
     return (
-        <>
-        <header className="header">
-            <Navigation />
-        </header>
-        <main>{children}</main>
-        <Footer />
-        </>
+        <div id="main-container">
+            <header id="main-header" className="header">
+                <h1>Portfolio</h1>
+                <Navigation />
+            </header>
+            <main>
+                {children}
+            </main>
+             <Footer />
+        </div>
     );
 }
