5a848bfee0f2d4f2fe40ecfeba26249313337b9c
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-10-16 12:28:51 +0200

    Update Navigation.tsx

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,12 +1,12 @@
 export default function Navigation() {
     return (
         <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/projects">Projects</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/projects">Projects</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
         </nav>
     );
 }
