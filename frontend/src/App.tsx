578082099c8972575607238604c902081a0ccea4
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-10-16 12:28:28 +0200

    Update App.tsx
    Added layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import "/src/styles/App.css";
 import ProjectsPage from "./features/projects/pages/index.tsx";
 import { IntroPage } from "./features/intro/pages/index.tsx";
+import Layout from "./components/Layout";
+
 
 
 
 function App() {
 	return (
-		<> 
+		<Layout>
 			<IntroPage />
 			<ProjectsPage />
-		</>
+		</Layout>
 	);
 }
 
