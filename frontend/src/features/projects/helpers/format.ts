0a4e20e1362fb28a6916f44e04cd35c85f5426b8
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-10-23 14:30:41 +0200

    Update format.ts
    From norwegian to english

--- frontend/src/features/projects/helpers/format.ts
+++ frontend/src/features/projects/helpers/format.ts
@@ -1,10 +1,14 @@
 import { formatDistance as fDistance} from 'date-fns';
-import { nb } from 'date-fns/locale';
+// import { nb } from 'date-fns/locale';
 
 export function formatDistance (updatedAt: Date): string {
     return fDistance(updatedAt, new Date(), {
         addSuffix: true,
         includeSeconds: true,
-        locale: nb,
+        // locale: nb, ---> Prefer english, but use this for norwegian
     });
+}
+
+export function capitalizeFirstLetter (str: string): string {
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
