493aead2bf7008292f5ba0d9c6368641ea6639ff
Espen Sørum <espensoerum@gmail.com>
2024-10-28 12:52:08 +0100

    Update db.ts
    Fixed setup for db

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,11 +1,11 @@
-// import { env } from "../lib/env";
-// import type { Database } from "better-sqlite3";
+import { env } from "../lib/env";
+import type { Database } from "better-sqlite3";
 
-// import BetterSqlite3 from "better-sqlite3";
+import BetterSqlite3 from "better-sqlite3";
 
-// // Lager en instans av databasen
-// export const db: Database = new BetterSqlite3(env.DATABASE_URL);
-// // Eksporterer databasen som en type så vi kan bruke den i repository
-// export type DB = typeof db;
+// Lager en instans av databasen
+export const db: Database = new BetterSqlite3(env.DATABASE_URL);
+// Eksporterer databasen som en type så vi kan bruke den i repository
+export type DB = typeof db;
 
-// export default db;
+export default db;
