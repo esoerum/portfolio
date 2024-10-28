5d044a7452c8decd5c5fb7ebda12f3dd84babc72
Espen Sørum <espensoerum@gmail.com>
2024-10-28 12:52:43 +0100

    Update app.ts
    Updated api and added patch backend logic for changes in a project. Lacking frontend part for now.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,6 @@ import { cors } from "hono/cors";
 import { UUID } from "node:crypto";
 import fs from "node:fs/promises";
 
-
 //Ha med V1 i /api/v1/projects
 
 type Project = {
@@ -20,42 +19,54 @@ const app = new Hono();
 app.use("/*", cors());
 
 // Route to get all projects
-app.get("/api/v1/projects", async (context) => {
+app.get("/api/v1/projects", async (ctx) => {
 	const data = await fs.readFile("./src/data/projects.json", "utf8");
 	const dataAsJson = JSON.parse(data);
-	return context.json(dataAsJson);
+	return ctx.json(dataAsJson);
 });
 
 //Route to get one project by id
-app.get("/api/v1/projects/:id", async (context) => {
-	const id = context.req.param("id");
+app.get("/api/v1/projects/:id", async (ctx) => {
+	const id = ctx.req.param("id");
 	const data = await fs.readFile("./src/data/projects.json", "utf8");
 	const projects = JSON.parse(data) as Project[];
 	const project = projects.find((project) => project.id === id);
-	return context.json(project);
+	return ctx.json(project);
 });
 
 // Route to add a new project
-app.post("/api/v1/projects", async (c) => {
+app.post("/api/v1/projects", async (ctx) => {
 	const data = await fs.readFile("./src/data/projects.json", "utf-8");
 	const projects = JSON.parse(data);
-	const project = await c.req.json() as Project;
+	const project = (await ctx.req.json()) as Project;
 	project.createdAt = new Date();
 	projects.push(project);
 	await fs.writeFile(
 		"./src/data/projects.json",
 		JSON.stringify(projects, null, 2)
 	);
-	return c.json(project);
-	
+	return ctx.json(project);
 });
 // Route to update a project
-// app.patch("/api/v1/projects/:id", async (c) => {
-
+app.patch("/api/v1/projects/:id", async (ctx) => {
+	const id = ctx.req.param("id");
+	const data = await fs.readFile("./src/data/projects.json", "utf-8");
+	const projects = JSON.parse(data) as Project[];
+	const project = (await ctx.req.json()) as Project;
+	const foundProject = projects.find((project) => project.id === id);
+	if (!foundProject) {
+		return ctx.json({ message: "Project not found" }, 400);
+	}
+	await fs.writeFile(
+		"./src/data/projects.json",
+		JSON.stringify(projects, null, 2)
+	);
+	return ctx.body(null, 204);
+});
 
 // Route to delete a project
-app.delete("/api/v1/projects/:id", async (c) => {
-	const id = c.req.param("id");
+app.delete("/api/v1/projects/:id", async (ctx) => {
+	const id = ctx.req.param("id");
 	const data = await fs.readFile("./src/data/projects.json", "utf-8");
 	const projects = JSON.parse(data) as Project[];
 	console.log(projects);
@@ -67,7 +78,7 @@ app.delete("/api/v1/projects/:id", async (c) => {
 		"./src/data/projects.json",
 		JSON.stringify(newProjects, null, 2)
 	);
-	return c.json({ message: "Project deleted" });
+	return ctx.json({ message: "Project deleted" });
 });
 
 export default app;
